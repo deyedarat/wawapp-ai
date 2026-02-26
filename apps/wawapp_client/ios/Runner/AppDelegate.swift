cdc8388be248cd8525341dd4d73f2d80c08c9d4a
deya2021 <86407832+deya2021@users.noreply.github.com>
2026-02-26 21:07:02 +0400

    Refactor AppDelegate for remote notifications handling

--- apps/wawapp_client/ios/Runner/AppDelegate.swift
+++ apps/wawapp_client/ios/Runner/AppDelegate.swift
@@ -6,43 +6,45 @@ import GoogleMaps
 @main
 @objc class AppDelegate: FlutterAppDelegate {
     override func application(
-          _ application: UIApplication,
-          didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-          FirebaseApp.configure()
-                    GMSServices.provideAPIKey("AIzaSyAWBq8WiUVzdS2fi7lG1RqV-YDz3DoJWoQ")
-          GeneratedPluginRegistrant.register(with: self)
-          application.registerForRemoteNotifications()
-          return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+        FirebaseApp.configure()
+        GMSServices.provideAPIKey("AIzaSyAWBq8WiUVzdS2fi7lG1RqV-YDz3DoJWoQ")
+        GeneratedPluginRegistrant.register(with: self)
+        application.registerForRemoteNotifications()
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
     // MARK: - APNs Token Registration
-
     override func application(
-          _ application: UIApplication,
-          didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
     ) {
-          Auth.auth().setAPNSToken(deviceToken, type: .unknown)
+        #if DEBUG
+        Auth.auth().setAPNSToken(deviceToken, type: .sandbox)
+        #else
+        Auth.auth().setAPNSToken(deviceToken, type: .prod)
+        #endif
     }
 
     override func application(
-          _ application: UIApplication,
-          didFailToRegisterForRemoteNotificationsWithError error: Error
+        _ application: UIApplication,
+        didFailToRegisterForRemoteNotificationsWithError error: Error
     ) {
-          print("Failed to register for remote notifications: \(error.localizedDescription)")
+        print("Failed to register for remote notifications: \(error.localizedDescription)")
     }
 
     // MARK: - Silent Push for Firebase Phone Auth
-
     override func application(
-          _ application: UIApplication,
-          didReceiveRemoteNotification userInfo: [AnyHashable: Any],
-          fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+        _ application: UIApplication,
+        didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+        fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
     ) {
-          if Auth.auth().canHandleNotification(userInfo) {
-                  completionHandler(.noData)
-                  return
-          }
-          completionHandler(.newData)
+        if Auth.auth().canHandleNotification(userInfo) {
+            completionHandler(.noData)
+            return
+        }
+        completionHandler(.newData)
     }
 }
